81703b77a92d25138e39453c08f6c923d3269bf7
Piyush Puranik <piyushpuranik@gmail.com>
2026-03-01 20:42:06 +0000

    Add data path getter and improve heroic getter logic

--- src/heroiclibrarymanager/core/environment.py
+++ src/heroiclibrarymanager/core/environment.py
@@ -1,6 +1,6 @@
 from functools import cached_property
 import platform
-from platformdirs import user_config_path
+from platformdirs import user_config_path, user_data_path
 from pathlib import Path
 import os
 
@@ -13,19 +13,32 @@ class Environment:
     @cached_property
     def heroic_config_root(self) -> Path | None:
         """ Config folder for heroic """
-        if self.os_name == "Linux":
-            paths = [
-                self.home / ".var/app/com.heroicgameslauncher.hgl/config/heroic",
-                self.home / ".config/heroic"
-            ]
-            for path in paths:
-                if path.exists():
-                    return path
+        if self.os_name != "Linux":
             return None
+        
+        xdg_base = os.environ.get("XDG_CONFIG_HOME")
+        if xdg_base:
+            native_path = Path(xdg_base) / "heroic"
+        else:
+            native_path = self.home / ".config/heroic"
+
+        flatpak_path = self.home / ".var/app/com.heroicgameslauncher.hgl/config/heroic"
+        if flatpak_path.exists():
+            return flatpak_path
+        if native_path.exists():
+            return native_path
+        return None
         
     @cached_property
     def app_config_root(self) -> Path | None:
         """ Config folder for this app """
         path = user_config_path("heroiclibrarymanager")
+        path.mkdir(parents=True, exist_ok=True)
+        return path
+    
+    @cached_property
+    def app_data_root(self) -> Path | None:
+        """ Data folder for this app """
+        path = user_data_path("heroiclibrarymanager")
         path.mkdir(parents=True, exist_ok=True)
         return path
